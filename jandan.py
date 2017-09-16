e5db2acd2ec204d1a0509c9f7844783f62ea49fd
henleySu1989 <steve.1989@163.com>
2017-09-16 14:13:02 +0800

    爬取图片并保存本地文件夹

---
+++ jandan.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import uuid
+import os
+import _thread
+
+imgPath = 'Image'
+threadFlag = True;
+def getimages(i):
+    url = 'http://jandan.net/ooxx/page-{}#comments'.format(str(i))
+    print(url)
+    web_data = requests.get(url)
+    Soup = BeautifulSoup(web_data.text, 'lxml')
+    images = Soup.select('.text > p > a')
+    if not os.path.isdir(imgPath):
+        os.makedirs(imgPath)
+    for image in images:
+        imgAdr = 'http:'+ image.get('href')
+        print(imgAdr)
+        imgName = imgPath+'/'+str(uuid.uuid1())+'.jpg'
+        urllib.request.urlretrieve(imgAdr,imgName)
+        print(imgName+'  '+'下载完成')
+
+start = eval(input("请输入起始页：\n"))
+end = eval(input("请输入终止页：\n"))
+print("开始下载")
+try:
+    for num in range(start,end+1):
+        _thread.start_new_thread(getimages,(num,))
+except:
+    print("无法进入线程")
+finally:
+    threadFlag = False
+while threadFlag:
+    pass
